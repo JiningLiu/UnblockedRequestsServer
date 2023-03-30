81b277fa123c5fa9b52385b7f68ff5d838e839c5
Jining Liu <88249105+JiningLiu@users.noreply.github.com>
2023-03-30 19:47:42 +0000

    added toString for inliner request

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ app.use(async (req, res, next) => {
                 res.send(
                     {
                         req: request,
-                        data: html,
+                        data: html.toString('urf-8'),
                     },
                 );
             });
